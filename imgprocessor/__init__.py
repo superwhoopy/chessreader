47787a3e89715c985a5294c1b971241b848c1eba
Emmanuel Ohayon <emmanuel.ohayon@gmail.com>
2015-10-10 00:32:24 +0200

    Playing with skimage: apply sobel

--- imgprocessor/__init__.py
+++ imgprocessor/__init__.py
@@ -1,4 +1,14 @@
+import time
+
+import skimage.io
+import skimage.filter
 
 def say_hello():
+    imarray = skimage.io.imread('imgprocessor/samples/flatboard.png', True)
+    edge_im = skimage.filter.sobel(imarray)
+
+    skimage.io.imshow(edge_im)
+
+    time.sleep(10)
     print("Hello, World!")
 
