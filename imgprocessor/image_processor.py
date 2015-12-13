84069ac0ede3de21d93d883dd4c7567b83995873
Daniel Ohayon <daniel@genestack.com>
2015-12-13 16:57:15 +0000

    improve logging, move edge detection to constructor, use SVM for color classification

--- imgprocessor/image_processor.py
+++ imgprocessor/image_processor.py
@@ -1,5 +1,4 @@
-from __future__ import division
-
+from __future__ import division, print_function
 import math
 import operator
 import os
@@ -7,11 +6,11 @@ import random
 
 import numpy as np
 import matplotlib.pyplot as plt
-from skimage import feature, io, color, filters, exposure
-from skimage.transform import hough_line, hough_line_peaks, resize
+from skimage import feature, io, color, exposure
+from skimage.transform import hough_line, hough_line_peaks
 from skimage.filters import threshold_otsu
 from sklearn.cluster import KMeans
-
+from sklearn.svm import SVC
 
 class ImageProcessor(object):
     """
@@ -29,12 +28,11 @@ class ImageProcessor(object):
     where each image corresponds to a single square in the chessboard.
 
     3. OCCUPANCY MATRIX ESTIMATION: we then determine for each square whether it is occupied or not.
-    To do this, we take the absolute difference between the current image and the first image of the game
-    (where all pieces are in their starting position).
+    To do this, we take the absolute difference between the current image and the image of the empty chessboard.
     All images are in grayscale for this step, and in order to detect black pieces on black squares,
     we use a gamma correction (with gamma < 1) on the input images and on the difference image.
     Then, we apply a binary thresholding to the image (using the Otsu method) and for each square,
-    we say that it is occupied if more than 10% of the pixels are white.
+    we say that it is occupied if more than 10% of its pixels are white after thresholding.
 
     4. BLINDBOARD ESTIMATION: for each occupied square, we pick a representative color (average RGB color
     in a disk centered on the midpoint of the square). We then use k-means clustering (k=2) on these data
@@ -46,62 +44,69 @@ class ImageProcessor(object):
     LINE_ANGLE_TOLERANCE = 0.1  # tolerance threshold (in radians) to filter horizontal and vertical lines
     TEMP_DIR = "temp_images"  # name of directory where intermediary images will be stored
 
-    def __init__(self, start_image_path):
+    def __init__(self, blank_image_path, start_image_path, verbose=False):
         """
         Class constructor. Should be called only once at the start of the game.
+        Computes chessboard edges (costly), and builds training data for color classification using the start image.
+
         Args:
-            start_image_path: path to the image where all the pieces are in their starting position.
+            blank_image_path: path to the image of the empty chessboard.
+            start_image_path: path to the image of the chessboard with all pieces in initial position.
+            verbose: if `True`, logging messages will be displayed and intermediary images
+                will be saved in the current directory inside the `temp_images` folder.
         """
 
-        self.start_image = io.imread(start_image_path)
+        self.empty_chessboard_image = io.imread(blank_image_path)
+        self.initial_image = io.imread(start_image_path)
         self.image = None
         self.n_rows, self.n_cols = None, None
-
+        self.temp_image_dir = None
         self._hough_lines = None
         self._edges = None
         self._square_images = None
         self._processed_square_images = None
         self._occupancy_matrix = None
         self._blindboard_matrix = None
-        self.show_details = None
+        self.verbose = verbose
+        self.color_classifier = None
 
-    def process(self, image_path, show_details=False, reference=None):
+        if verbose:
+            if not os.path.isdir(self.TEMP_DIR):
+                os.mkdir(self.TEMP_DIR)
+            print("Computing chessboard edges...")
+
+        self._edges, self._hough_lines = self.find_edges(self.empty_chessboard_image)
+
+        if verbose:
+            self.plot_chessboard_with_edges(self.empty_chessboard_image)
+
+        self.train_color_classifier()
+
+    def process(self, image_path, reference=None):
         """
         Run the image analysis pipeline to compute the blindboard.
 
         Args:
             image_path: path to image to analyze
-            show_details: if `True`, logging messages will be displayed and intermediary images
-                will be saved in the current directory inside the `temp_images` folder.
             reference: (optional) a `numpy.array` storing the "actual" blindboard. If provided,
                 accuracy metrics will be displayed.
-
         """
 
-        self.show_details = show_details
-        if show_details:
-            print("Processing `{0}`...".format(os.path.basename(image_path)))
-            if not os.path.isdir(self.TEMP_DIR):
-                os.mkdir(self.TEMP_DIR)
-
+        if self.verbose:
+            self.temp_image_dir = os.path.join(self.TEMP_DIR,
+                                               os.path.basename(image_path).rsplit(".", 1)[0])
+            if not os.path.isdir(self.temp_image_dir):
+                os.makedirs(self.temp_image_dir)
+                print("Processing `{0}`...".format(os.path.basename(image_path)))
 
         self.image = io.imread(image_path)
-        self.n_rows, self.n_cols = self.image.shape[:2]
+        self.n_rows, self.n_cols = self.image.shape[:2]  # TODO remove this
 
-        if show_details:
+        if self.verbose:
             self.save_image("image.jpg", self.image)
-            print("Computing edges...")
-
-        self._edges = self.find_edges()
-
-        if show_details:
-            self.plot_chessboard_with_edges()
             print("Computing occupancy matrix...")
 
         self.compute_occupancy_matrix()
-
-        # if show_details:
-        #     self.plot_square_images(self._processed_square_images)
 
         if reference is not None:
             print("OCCUPANCY MATRIX ESTIMATION")
@@ -109,7 +114,7 @@ class ImageProcessor(object):
             false_negatives = np.sum(~self._occupancy_matrix & reference)
             print("False positives:", false_positives, "- False negatives:", false_negatives)
 
-        if show_details:
+        if self.verbose:
             print("Computing blindboard matrix...")
         self._blindboard_matrix = self.compute_blindboard_matrix()
 
@@ -119,22 +124,23 @@ class ImageProcessor(object):
                                     np.sum(self._blindboard_matrix != ~reference)))
             print("Number of mislabeled pieces:", number_errors)
 
-        if show_details:
+        if self.verbose:
             print("Blindboard matrix:")
             print(self._blindboard_matrix)
 
-    def compute_canny_image(self):
-        image_gray = exposure.equalize_hist(color.rgb2gray(self.image))
+    @staticmethod
+    def compute_canny_image(image):
+        image_gray = exposure.equalize_hist(color.rgb2gray(image))
         image_canny = np.zeros_like(image_gray)
         for i in range(3):
-            normalized_image = self.image[:, :, i]
+            normalized_image = image[:, :, i]
             image_canny = np.logical_or(image_canny, feature.canny(normalized_image, 1))
         return image_canny
 
-    def find_edges(self):
-        image_canny = self.compute_canny_image()
+    def find_edges(self, image):
+        image_canny = self.compute_canny_image(image)
         h, theta, d = hough_line(image_canny)
-        min_distance = int(math.floor(self.image.shape[1] / 11))  # TODO better way?
+        min_distance = int(math.floor(image.shape[1] / 11))  # TODO better way?
         hough_peaks = hough_line_peaks(h, theta, d, min_distance=min_distance, threshold=40)  # TODO adjust
 
         lines = {'horizontals': [], 'verticals': []}
@@ -159,46 +165,65 @@ class ImageProcessor(object):
                 denum = np.cos(theta1) * np.sin(theta2) - np.sin(theta1) * np.cos(theta2)
                 x_inter = (r1 * np.sin(theta2) - r2 * np.sin(theta1)) / denum
                 y_inter = (r2 * np.cos(theta1) - r1 * np.cos(theta2)) / denum
-                if 0 < x_inter < self.n_cols and 0 < y_inter < self.n_rows:
+                if 0 < x_inter < image.shape[1] and 0 < y_inter < image.shape[0]:
                     intersections.append((x_inter, y_inter))
 
-        self._hough_lines = lines['horizontals'] + lines['verticals']
+        hough_lines = lines['horizontals'] + lines['verticals']
         # TODO we don't check that we get 9x9 intersections...
-        return np.array(intersections)
+        return np.array(intersections), hough_lines
 
-    def cut_squares(self, input_image):
+    def train_color_classifier(self):
+
+        self.color_classifier = SVC()
+        initial_squares = self.cut_squares(self.initial_image, self._edges)
+        training_data = np.zeros((32, 3))  # 32 pieces (16 black, 16 white), 3 color channels
+        training_labels = [-1 for _ in range(16)] + [1 for _ in range(16)]
+        for i in range(2):
+            for j in range(8):
+                square = initial_squares[i,j]
+                l_x, l_y = square.shape[:2]
+                x, y = np.ogrid[:l_x, :l_y]
+                disk_mask = (x - l_x / 2)**2 + (y - l_y / 2)**2 <= (l_x / 4)**2
+                # TODO here : retrieve the mean for each dimension
+                np.mean(square[disk_mask], axis=0)
+                training_data[i*8+j, :] = means
+        # TODO here: train color classifier
+
+    @staticmethod
+    def cut_squares(input_image, edges):
         """
         Takes as input a chessboard image and uses the estimated square edges `self._edges` to
         cut the image into 64 square images.
         Args:
             input_image: image to cut
+            edges: `numpy.array` with edges coordinates
 
         Returns: an 8x8 matrix where each entry is a square image (with the same dtype as `input_image`)
         """
         images_matrix = np.empty((8, 8), dtype=object)
-        edges_matrix = np.reshape(self._edges, (9, 9, 2))
+        edges_matrix = np.reshape(edges, (9, 9, 2))
         for i in range(8):
             for j in range(8):
                 top_left = edges_matrix[i][j]
                 bottom_right = edges_matrix[i + 1][j + 1]
                 square_image = input_image[math.floor(top_left[1]):math.ceil(bottom_right[1]),
-                                          math.floor(top_left[0]):math.ceil(bottom_right[0])]
+                               math.floor(top_left[0]):math.ceil(bottom_right[0])]
                 images_matrix[i, j] = square_image
         return images_matrix
 
-    def plot_chessboard_with_edges(self):
-        # fig = plt.figure()
-        plt.imshow(self.image, cmap=plt.cm.gray)
+    def plot_chessboard_with_edges(self, chessboard_image):
+        plt.imshow(chessboard_image, cmap=plt.cm.gray)
+        n_rows, n_cols = chessboard_image.shape[:2]
 
         for _, angle, dist in self._hough_lines:
             y0 = (dist - 0 * np.cos(angle)) / np.sin(angle)
-            y1 = (dist - self.n_cols * np.cos(angle)) / np.sin(angle)
-            plt.plot((0, self.n_cols), (y0, y1), '-r')
+            y1 = (dist - n_cols * np.cos(angle)) / np.sin(angle)
+            plt.plot((0, n_cols), (y0, y1), '-r')
 
         for i, edge in enumerate(self._edges, 1):
             plt.text(edge[0], edge[1], str(i), color='blue')
 
-        plt.axis((0, self.n_cols, self.n_rows, 0))
+        plt.axis((0, n_cols, n_rows, 0))
         plt.savefig(os.path.join(self.TEMP_DIR, "edges_and_squares.jpg"))
 
     @staticmethod
@@ -211,36 +236,38 @@ class ImageProcessor(object):
         plt.plot()
         plt.show()
 
-    def save_image(self, name, image):
-        plt.imsave(os.path.join(self.TEMP_DIR, name), image, cmap=plt.cm.gray)
+    def save_image(self, name, image, path=None):
+        if path is None:
+            path = self.temp_image_dir
+        plt.imsave(os.path.join(path, name), image, cmap=plt.cm.gray)
 
     def compute_occupancy_matrix(self):
         self._occupancy_matrix = np.empty((8, 8), dtype=bool)
         self._occupancy_matrix.fill(False)
 
         # compute difference between starting and current image
-        adj_start_image = exposure.adjust_gamma(color.rgb2gray(self.start_image), 0.1)
+        adj_start_image = exposure.adjust_gamma(color.rgb2gray(self.empty_chessboard_image), 0.1)
         adj_image = exposure.adjust_gamma(color.rgb2gray(self.image), 0.1)
         diff_image = exposure.adjust_gamma(np.abs(adj_image - adj_start_image), 0.5)
         binary_diff_image = diff_image > threshold_otsu(diff_image)
 
         # FIXME use a different correction to detect white pieces
-        if self.show_details:
+        if self.verbose:
             self.save_image("gamma_adj_image.jpg", adj_image)
             self.save_image("diff_gray.jpg", diff_image)
             self.save_image("diff_bw.jpg", binary_diff_image)
 
-        binary_diff_squares = self.cut_squares(binary_diff_image)
+        binary_diff_squares = self.cut_squares(binary_diff_image, self._edges)
 
         self._processed_square_images = np.empty((8, 8), dtype=np.ndarray)
 
         for i in range(binary_diff_squares.shape[0]):
             for j in range(binary_diff_squares.shape[1]):
-                square = binary_diff_squares[i,j]
+                square = binary_diff_squares[i, j]
                 n_pixels = square.shape[0] * square.shape[1]
-                self._occupancy_matrix[i,j] = np.sum(square)/n_pixels > 0.1
+                self._occupancy_matrix[i, j] = np.sum(square) / n_pixels > 0.1
                 # pdb.set_trace()
-                self._processed_square_images[i,j] = square
+                self._processed_square_images[i, j] = square
 
     @staticmethod
     def get_representative_color(img):
@@ -257,7 +284,7 @@ class ImageProcessor(object):
         return output
 
     def compute_blindboard_matrix(self):
-        square_images = self.cut_squares(self.image)
+        square_images = self.cut_squares(self.image, self._edges)
         square_colors = self.apply_to_matrix(self.get_representative_color, square_images, 3)
         square_colors = np.reshape(square_colors, (64, 3))
         occupied_squares = np.reshape(self._occupancy_matrix, (64,))
@@ -274,7 +301,8 @@ class ImageProcessor(object):
 if __name__ == "__main__":
     IMAGES_FOLDER = '/Users/daniel/Desktop/chess/chess-pictures-other'
     base_img = os.path.join(IMAGES_FOLDER, 'Picture 13.jpg')
+    start_img = os.path.join(IMAGES_FOLDER, 'Picture 14.jpg')
     test_files = [os.path.join(IMAGES_FOLDER, f) for f in os.listdir(IMAGES_FOLDER)
-                  if f.endswith(".jpg") and f != "Picture 13.jpg"]
-    processor = ImageProcessor(base_img)
-    processor.process(random.choice(test_files), show_details=True)
+                  if f.endswith(".jpg") and f not in {'Picture 13.jpg', 'Picture 14.jpg'}]
+    processor = ImageProcessor(base_img, start_img, verbose=True)
+    processor.process(random.choice(test_files))
